74510cf669810901df2003dfabec5ed3a9b4ac88
Shane Hanna <shane.hanna@gmail.com>
2009-05-26 12:10:24 +1000

    Test::Unit

--- config/init.rb
+++ config/init.rb
@@ -3,13 +3,13 @@
 require 'config/dependencies.rb'
  
 use_orm :datamapper
-use_test :rspec
+use_test :test_unit
 use_template_engine :erb
  
 Merb::Config.use do |c|
   c[:use_mutex] = false
   c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
-  
+
   # cookie session store configuration
   c[:session_secret_key]  = '87107539db8cfa483be5b146200d88adb071300e'  # required for cookie session store
   c[:session_id_key] = '_oursignal/_session_id' # cookie session id key, defaults to "_session_id"
