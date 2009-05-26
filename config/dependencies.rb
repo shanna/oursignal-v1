21abee91bc5ee4a9ea0f44bbe60b5ba4eaa544c4
Shane Hanna <shane.hanna@gmail.com>
2009-05-26 12:08:27 +1000

    Frozen gems.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,25 +1,21 @@
-merb_gems_version = "1.1"
-dependency "merb-core", merb_gems_version 
-dependency "merb-assets", merb_gems_version
-dependency("merb-cache", merb_gems_version) do
+merb_gems_version = '1.0.7.1'
+dependency 'merb-core', merb_gems_version 
+dependency 'merb-assets', merb_gems_version
+dependency('merb-cache', merb_gems_version) do
   Merb::Cache.setup do
     register(Merb::Cache::FileStore) unless Merb.cache
   end
 end
-dependency "merb-helpers", merb_gems_version
-# dependency "merb-slices", merb_gems_version
-# dependency "merb-auth-core", merb_gems_version
-# dependency "merb-auth-more", merb_gems_version
-# dependency "merb-exceptions", merb_gems_version
-# dependency "merb-gen", merb_gems_version
+dependency 'merb-helpers', merb_gems_version
+dependency 'merb-exceptions', merb_gems_version
 
-do_gems_version = "0.9.12"
-dependency "data_objects", do_gems_version
+do_gems_version = '0.9.12'
+dependency 'data_objects', do_gems_version
 
-dm_gems_version = "0.9.11"
-dependency "dm-core", dm_gems_version
+dm_gems_version = '0.9.11'
+dependency 'dm-core', dm_gems_version
 
-dependency "merb_datamapper", merb_gems_version
+dependency 'merb_datamapper', merb_gems_version
 
 # Github
 dependency 'shanna-dm-tokyo-cabinet-adapter', '0.1.6', :require_as => 'dm-tokyo-cabinet-adapter'
