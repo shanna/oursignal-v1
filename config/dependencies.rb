207d331ed0c195fd63eaab100d9cc10a760aecce
Shane Hanna <shane.hanna@gmail.com>
2009-06-01 14:27:49 +1000

    Edge.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,5 +1,11 @@
-# bin/merb -V to get a better error messages.
 merb_gems_version = '1.0.11'
+do_gems_version   = '0.9.13'
+dm_gems_version   = '0.10.0'
+
+Merb::DO_VERSION = do_gems_version
+Merb::DM_VERSION = dm_gems_version
+
+# bin/merb -V to get a better error messages.
 dependency 'merb-core', merb_gems_version
 dependency 'merb-assets', merb_gems_version
 dependency('merb-cache', merb_gems_version) do
@@ -10,20 +16,20 @@ end
 dependency 'merb-helpers', merb_gems_version
 dependency 'merb-exceptions', merb_gems_version
 
-do_gems_version = '0.9.12'
-dm_gems_version = '0.9.11'
 dependency 'data_objects', do_gems_version
 dependency 'dm-core', dm_gems_version
-dependency 'merb_datamapper', merb_gems_version
 
 # Github
 
-# Requires tokyocabinet.
 # http://transact.dl.sourceforge.net/sourceforge/tokyocabinet/tokyocabinet-1.4.23.tar.gz
-# http://transact.dl.sourceforge.net/sourceforge/tokyocabinet/tokyocabinet-ruby-1.25.tar.gz
-# TODO: Dunno what it is but the ruby bindings have a lot of trouble finding the shared TC libs.
-# I ended up symlinking /usr/lib/libtokyocabinet.so.8 to /usr/local/lib/tokyocabinet.so.8
-dependency 'shanna-dm-tokyo-cabinet-adapter', '0.1.6', :require_as => 'dm-tokyo-cabinet-adapter'
+dependency 'shanna-dm-tokyo-adapter', '0.2.1', :require_as => 'dm-tokyo-adapter' do
+  # TODO: Replace with merb_datamapper once Merb 1.1 and DM 0.10.0 is released.
+  DataMapper.setup(:default,
+    :adapter  => 'tokyo_cabinet',
+    :database => 'tc',
+    :path     => Merb.root
+  )
+end
 
 # Thor/other libs.
 dependency 'pauldix-feedzirra', '0.0.12', :require_as => nil
--- config/init.rb
+++ config/init.rb
@@ -1,11 +1,10 @@
 # Go to http://wiki.merbivore.com/pages/init-rb
- 
+
 require 'config/dependencies.rb'
- 
-use_orm :datamapper
+
 use_test :test_unit
 use_template_engine :erb
- 
+
 Merb::Config.use do |c|
   c[:use_mutex] = false
   c[:session_store] = 'cookie'  # can also be 'memory', 'memcache', 'container', 'datamapper
@@ -14,11 +13,11 @@ Merb::Config.use do |c|
   c[:session_secret_key]  = '87107539db8cfa483be5b146200d88adb071300e'  # required for cookie session store
   c[:session_id_key] = '_oursignal/_session_id' # cookie session id key, defaults to "_session_id"
 end
- 
+
 Merb::BootLoader.before_app_loads do
   # This will get executed after dependencies have been loaded but before your app's classes have loaded.
 end
- 
+
 Merb::BootLoader.after_app_loads do
   # This will get executed after your app's classes have been loaded.
 end
