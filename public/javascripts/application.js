568997a98bcaccaf74aa91bc1a76386eee572fe9
Shane Hanna <shane.hanna@gmail.com>
2009-06-07 02:55:55 +1000

    Fade notices out. Will be replaced by a growl type deal later?

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,18 @@
 // Global JS.
+
+(function ($) {
+  $.extend($.fn, {
+    notice: function (options) {
+      var defaults = {delay: 5000};
+      options = $.extend(defaults, options);
+
+      return this.each(function () {
+        $(this).animate({opacity: 100}, options.delay).slideUp('slow', this.remove);
+      });
+    }
+  });
+
+  $(document).ready(function () {
+    $('#notice').notice();
+  });
+})(jQuery);
