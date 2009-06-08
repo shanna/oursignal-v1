e6b1d993a530954ed9bd8e70cefec183c0d85d3a
Shane Hanna <shane.hanna@gmail.com>
2009-06-09 00:24:09 +1000

    Use white as base text colour for now.

--- config/router.rb
+++ config/router.rb
@@ -2,6 +2,7 @@ Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do
   resources :users
   resources :feeds
+  resources :themes # TODO: Not a RESTful resource.
 
   # Keep in mind the names are required by merb-auth-more.
   # TODO: merb-auth has a massive stink about it. Not as nice as the rest of Merb.
@@ -9,6 +10,6 @@ Merb::Router.prepare do
   match('/openid/logout').to(:controller => :users, :action => 'logout').name(:logout)
   match('/openid/signup').to(:controller => :open_id, :action => 'signup').name(:signup)
 
-  # Change this for your home page to be available at /
+
   match('/').to(:controller => :themes, :action => 'index')
 end
