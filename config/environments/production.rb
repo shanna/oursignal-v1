93988310637010cd0a57d1dba2416c5558f21c56
Danial Pearce <git@tigris.id.au>
2009-08-20 10:26:46 +1000

    Add rubyinline fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 Merb.logger.info("Loaded PRODUCTION Environment...")
+ENV['INLINEDIR'] = Merb.root / 'tmp'
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes] = false
   c[:log_level] = :error
-  
   c[:log_file]  = Merb.root / "log" / "production.log"
   # or redirect logger using IO handle
   # c[:log_stream] = STDOUT
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,9 +1,9 @@
 Merb.logger.info("Loaded STAGING Environment...")
+ENV['INLINEDIR'] = Merb.root / 'tmp'
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes]    = false
   c[:log_level]         = :error
-
   c[:log_file]  = Merb.root / "log" / "staging.log"
   # or redirect logger using IO handle
   # c[:log_stream] = STDOUT
