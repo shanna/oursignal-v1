12a194f47529a3165e52096ef86f18fa1a6e7892
Shane Hanna <shane.hanna@gmail.com>
2009-08-21 11:10:26 +1000

    Use google jquery, jquery-ui. Turn on asset bundling in staging.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,7 @@
 Merb.logger.info("Loaded STAGING Environment...")
 ENV['INLINEDIR'] = Merb.root / 'tmp'
 Merb::Config.use { |c|
+  c[:bundle_assets]     = true
   c[:exception_details] = false
   c[:reload_classes]    = false
   c[:log_level]         = :error
