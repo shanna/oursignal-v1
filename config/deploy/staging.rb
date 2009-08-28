10d1363e13b0608f4a2968231b5067f84663dca8
Danial Pearce <git@tigris.id.au>
2009-08-28 15:52:10 +1000

    Restrict apache to office

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,13 @@ role :web, 'staging.oursignal.com'
 role :app, 'staging.oursignal.com'
 role :db, 'staging.oursignal.com', :primary => true
 set :deploy_to, '/srv/staging.oursignal.com'
+
+set :apache_config_extra, <<__CONFIG__
+  RackEnv staging
+
+  <Directory #{deploy_to}>
+    Order deny,allow
+    Deny from all
+    Allow from 203.206.182.106 # office
+  </Directory>
+__CONFIG__
