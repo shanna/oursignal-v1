afc11f780d486063af21e4d050a80327162e2568
Shane Hanna <shane.hanna@gmail.com>
2009-09-01 11:00:15 +1000

    Include domains in list theme meta.

--- public/themes/list/theme.js
+++ public/themes/list/theme.js
@@ -8,7 +8,8 @@
         $.links().map(function (link) {
           var score    = $('<div class="score" />').append('score: ', link.score);
           var velocity = $('<div class="velocity" />').append('velocity: ', link.velocity);
-          var meta     = $('<div class="meta" />').append(score, velocity);
+          var domains  = $('<div class="domains" />').append('domains: ', (link.domains || []).join(', '));
+          var meta     = $('<div class="meta" />').append(score, velocity, domains);
           var anchor   = $('<a />').attr({href: link.url}).append(link.title);
           ul.append($('<li />').append(anchor, meta));
         });
