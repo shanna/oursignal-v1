b7f67a7782c7af3ef2ba0fcaa58e1df6b033cd99
Shane Hanna <shane.hanna@gmail.com>
2009-09-02 16:45:45 +1000

    Doh. Conditional was back to front.

--- app/models/link.rb
+++ app/models/link.rb
@@ -63,7 +63,7 @@ class Link
         if referred_at = (entry.published.to_datetime rescue nil)
           link.referred_at = referred_at if link.referred_at.blank? || link.referred_at < referred_at
         else
-          link.referred_at = DateTime.now unless link.referred_at.blank?
+          link.referred_at = DateTime.now if link.referred_at.blank?
         end
 
         link.save && feed.feed_links.first_or_create({:link => link}, :url => url)
