dd13604b3602cdc27a3e364f32febcdc8041c362
Shane Hanna <shane.hanna@gmail.com>
2009-09-15 13:38:43 +1000

    Fix digg feed links so they point to the real url.

--- lib/oursignal/score/source/digg.rb
+++ lib/oursignal/score/source/digg.rb
@@ -13,10 +13,19 @@ module Oursignal
         end
 
         def work(data = '')
-          # TODO: Create fake feed entries for ./@link in digg popular feed.
           Nokogiri::XML.parse(data).xpath('//story').each do |story|
-            score(story.at('./@link').text, story.at('./@diggs').text.to_i)
-            score(story.at('./@href').text, story.at('./@diggs').text.to_i)
+            real_url = URI.sanatize(story.at('./@link').text)
+            digg_url = URI.sanatize(story.at('./@href').text)
+
+            # Fake a meta entry and force a meta update to fix digg RSS feed links as metauri.com doesn't
+            # (and shouldn't) follow links from these sites.
+            # TODO: I should build something more general on top of URI::Meta to remove all these known aggregator sites?
+            meta = URI::Meta.new(:uri => digg_url, :last_effective_uri => real_url)
+            URI::Meta::Cache.store(digg_url, meta)
+            Link.repository.adapter.execute(%q{update links set meta_at = null where url = ?}, digg_url)
+
+            score(digg_url, story.at('./@diggs').text.to_i)
+            score(real_url, story.at('./@diggs').text.to_i)
           end
         end
       end # Digg
