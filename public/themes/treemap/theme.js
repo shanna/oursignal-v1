9ddf8806b9c3ab0eaecb503b70693725428e3c11
Shane Hanna <shane.hanna@gmail.com>
2009-09-30 12:29:34 +1000

    IE8 doesn't like css styles being set before the element exists in the DOM I think.

--- public/themes/treemap/theme.js
+++ public/themes/treemap/theme.js
@@ -61,7 +61,7 @@
         for (var i = 0; i < links.length; i++) {
           var link     = links[i];
           var velocity = $('<div class="velocity" />').append(link.velocity);
-          var meta     = $('<div class="meta" style="display: none;" />').append(velocity);
+          var meta     = $('<div class="meta" />').append(velocity);
           var anchor   = $('<a />').attr({href: link.url}).append(link.title);
           var el       = $('<span />').append(meta, anchor);
           $.fn.visualize.links.push([el, parseFloat(link.score) * 100]);
