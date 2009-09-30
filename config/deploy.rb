9960f6f0dc4ee69320430200071dcee8679d132b
Danial Pearce <git@tigris.id.au>
2009-10-01 00:48:52 +1000

    Make sure monit looks at it's special controller/action

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,8 +28,8 @@ set :gems, fetch(:gems, []).push('jeweler')
 # Change the apache port because we run a caching service (varnish) on port 80
 set :apache_port, '8080'
 
-set :apache_monit_test_urls, ['http://staging.oursignal.com:8080/']
-set :varnish_monit_test_urls, ['http://staging.oursignal.com/']
+set :apache_monit_test_urls, ['http://staging.oursignal.com:8080/static/monit']
+set :varnish_monit_test_urls, ['http://staging.oursignal.com/static/monit']
 
 set(:default_environment) do
   { 'MERB_ENV' => stage }
