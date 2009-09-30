b7769cdc29cb082f22eefb21755b35a8f455c061
Danial Pearce <git@tigris.id.au>
2009-09-30 23:11:00 +1000

    Fix bug in deployment

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -54,4 +54,4 @@ dependency 'uri-meta', '0.8.8', :require_as => 'uri/meta'
 # These are for deployment only, they will be required when needed
 dependency 'capistrano', '2.5.8', :require_as => nil
 dependency 'capistrano-ext', '1.2.1', :require_as => nil
-dependency 'stateless-systems-capistrano-ext', '0.10.4', :require_as => nil
+dependency 'stateless-systems-capistrano-ext', '0.10.5', :require_as => nil
