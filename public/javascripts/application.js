61a5c7553e4af2c759bde2fa774cd37e404df0dc
Shane Hanna <shane.hanna@gmail.com>
2009-10-01 14:38:50 +1000

    Notice code no longer used.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,6 @@
 (function ($) {
   $.os = {};
 
-  // TODO: Replace with growl type notifications plugin.
-  $.extend($.fn, {
-    notice: function (options) {
-      var defaults = {delay: 5000};
-      options = $.extend(defaults, options);
-
-      return this.each(function () {
-        $(this).animate({opacity: 100}, options.delay).slideUp('slow', this.remove);
-      });
-    }
-  });
-
   // TODO: Namespace everything?
   $.extend($, {
     // Last authorized username.
@@ -39,9 +27,5 @@
       }
       return $.os.links;
     }
-  });
-
-  $(document).ready(function () {
-    $('#notice').notice();
   });
 })(jQuery);
