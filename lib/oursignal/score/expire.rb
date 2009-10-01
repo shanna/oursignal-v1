2a4b90e4b32b6ba45bd2103691648370c07ab606
Shane Hanna <shane.hanna@gmail.com>
2009-10-01 13:28:55 +1000

    Fixed naming bug in expire job.

--- lib/oursignal/score/expire.rb
+++ lib/oursignal/score/expire.rb
@@ -6,7 +6,7 @@ module Oursignal
       def call
         scores = ::Score.all(:created_at.lt => (Time.now - 1.week).to_datetime)
         unless scores.empty?
-          Merb.logger.info("expire\tdeleting expired #{links.size} scores")
+          Merb.logger.info("expire\tdeleting expired #{scores.size} scores")
           scores.each(&:destroy)
         end
       end
