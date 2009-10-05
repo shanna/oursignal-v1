26708e35a927f6d7a222876b64a932a66cad5d83
Shane Hanna <shane.hanna@gmail.com>
2009-10-05 12:15:37 +1100

    First cut of tooltips.

--- public/themes/treemap/theme.js
+++ public/themes/treemap/theme.js
@@ -1,7 +1,8 @@
 (function ($) {
   $.require([
     '/themes/treemap/jquery-treemap.js',
-    '/themes/treemap/jquery-textfill.js'
+    '/themes/treemap/jquery-textfill.js',
+    '/themes/treemap/jquery-simpletip.js'
   ]);
 
   $.extend($.fn, {
@@ -17,14 +18,21 @@
         var links    = $(this);
         links.children().remove();
         links.treemap(options.width, options.height, {getData: data(options)});
-        links.find('div.treemapCell span').textfill({max: 50}).velocity();
+        links.find('div.treemapCell span').link_context().textfill({max: 50}).velocity().tooltip();
+      });
+    },
+
+    link_context: function () {
+      return this.each(function () {
+        var el = $(this);
+        if (el.data('link')) el.context.link = el.data('link');
       });
     },
 
     velocity: function () {
       return this.each(function () {
         var el  = $(this);
-        var vel = parseFloat(el.find('.meta .velocity').text());
+        var vel = parseFloat(el.context.link.velocity);
 
         var colour = '303030';
         if (vel < 1)   colour = "eb433a";
@@ -39,6 +47,34 @@
         if (vel < -0.9)  colour = "42788c";
 
         el.parent().css('background-color', '#' + colour);
+      });
+    },
+
+    tooltip: function () {
+      return this.each(function () {
+        var el = $(this);
+        el.parent().simpletip({
+          offset:       [20, 20],
+          content:      '',
+          fixed:        false,
+          onBeforeShow: function () {
+            if (this.getTooltip().text() == '') {
+              var link       = el.context.link;
+              var title      = $('<div class="title" />').append(link.title);
+              var screenshot = $('<div class="screenshot" />').append($('<img />').attr({
+                width:  '120',
+                height: '90',
+                src:    'http://open.thumbshots.org/image.aspx?url=' + escape(link.url)
+              }));
+              var url        = $('<div class="url" />').append('url: ' + link.url);
+              var score      = $('<div class="score" />').append('score: ' + link.score);
+              var velocity   = $('<div class="velocity" />').append('velocity: ' + link.velocity);
+              var domains    = $('<div class="domains" />').append('via: ' + (link.domains || []).join(', '));
+              var meta       = $('<div class="meta" />').append(title, screenshot, url, score, velocity, domains);
+              this.update(meta);
+            }
+          }
+        });
       });
     }
   });
@@ -60,10 +96,10 @@
         $.fn.visualize.links = []
         for (var i = 0; i < links.length; i++) {
           var link     = links[i];
-          var velocity = $('<div class="velocity" />').append(link.velocity);
-          var meta     = $('<div class="meta" />').append(velocity);
-          var anchor   = $('<a />').attr({href: link.url, title: link.url}).append(link.title);
-          var el       = $('<span />').append(meta, anchor);
+          var anchor   = $('<a />').attr({href: link.url, title: ''}).append(link.title);
+          var el       = $('<span />').append(anchor).data('link', link);
+          // el.context.link = link;
+          // console.warn(el.context);
           $.fn.visualize.links.push([el, parseFloat(link.score) * 100]);
         };
       }
