1563f9650d491a9c4c80c41a8c81b40bcc9dfb27
Shane Hanna <shane.hanna@gmail.com>
2009-10-08 11:48:32 +1100

    Set feed_links.external on feed updates.

--- app/models/feed_link.rb
+++ app/models/feed_link.rb
@@ -1,10 +1,11 @@
 class FeedLink
-    include DataMapper::Resource
-    property :feed_id, String, :key => true, :nullable => false, :length => 40
-    property :link_id, String, :key => true, :nullable => false, :length => 40
-    property :url,     String, :length => 255, :index => true
+  include DataMapper::Resource
+  property :feed_id,  String, :key => true, :nullable => false, :length => 40
+  property :link_id,  String, :key => true, :nullable => false, :length => 40
+  property :url,      String, :length => 255, :index => true
+  property :external, Boolean, :default => false
 
-    belongs_to :feed
-    belongs_to :link
+  belongs_to :feed
+  belongs_to :link
 end # FeedLink
 
