c94ce40cbe41f47a3af3efd0fc23df9c5f68bc3a
Shane Hanna <shane.hanna@gmail.com>
2009-10-15 16:03:24 +1100

    Task to look at a users feed numbers.

--- tasks/os.thor/user.rb
+++ tasks/os.thor/user.rb
@@ -7,14 +7,14 @@ module Os
       super(*args)
     end
 
-    desc 'links', %q{Links for a username.}
+    desc 'links(username)', %q{Links for a username.}
     def links(username)
+      DataMapper.logger.level = :error
       user  = ::User.first(:username => username)
-      links = user.links.to_a
 
-      puts 'id       | score_avg(sa) | score_bonus(sb) | (sa * sb) | age(a)    | (sa * sb * a) | score    | velocity_avg | velocity'
+      puts 'link_id  | score_avg(sa) | score_bonus(sb) | (sa * sb) | age(a)    | (sa * sb * a) | score    | velocity_avg | velocity'
       puts '---------+---------------+-----------------+-----------+-----------+---------------+----------+--------------+---------'
-      links.each do |l|
+      user.links.each do |l|
         puts(
           '%.8s | %+.5f      | %+.5f        | %+.5f  | %+.5f  | %+.5f      | %+.5f | %+.5f     | %+.5f' %
           [
@@ -27,6 +27,29 @@ module Os
             l.score,
             l.velocity_average,
             l.velocity
+          ]
+        )
+      end
+    end
+
+    desc 'feeds(username)', %q{Feeds for username.}
+    def feeds(username)
+      DataMapper.logger.level = :error
+      user  = ::User.first(:username => username)
+      links = user.links.to_a
+
+      puts 'feed_id  | links | user_score | user_links | domain'
+      puts '---------+-------+------------+------------+-------'
+      user.user_feeds.each do |uf|
+        count = links.find_all{|l| l.domains.include?(uf.feed.domain)}.size
+        puts('%.8s | %5d |    %.5f |   %2d (%2d%%) | %s' %
+          [
+            uf.feed_id,
+            uf.feed.links.count,
+            uf.score,
+            count,
+            (count.to_f / links.size) * 100,
+            uf.feed.domain
           ]
         )
       end
