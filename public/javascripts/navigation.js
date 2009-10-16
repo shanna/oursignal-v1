21652d6bfbf315bd021f1f985071c069e758025b
Simon Richardson <simon@statelesssystems.com>
2009-10-16 17:13:54 +1100

    changing margins on header

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -24,13 +24,13 @@
 
     // Logout, Customize
     var el_options = $('#head .navigation .options');
-    var el_logout  = $('<a />').attr('href', '/users/' + username + '/logout').append(username, ': ', 'Logout ');
+    var el_logout  = $('<a />').attr('href', '/users/' + username + '/logout').append('Log out ');
     el_options.find('.customize a').attr('href', '/users/' + username + '/edit');
     el_options.prepend($('<li />').attr('class', 'logout').append(el_logout));
 
     // Username
-    var el_username   = $('<a />').attr({href: '/' + username + '/', 'class': 'username'}).append(username);
-    var el_user       = $('<li />').attr('class', 'user').append(el_username, ' &raquo; ');
+    var el_username   = $('<a />').attr({href: '/' + username + '/', 'class': 'username'}).append('Hello, ',username,'!');
+    var el_user       = $('<li />').attr('class', 'user').append(el_username);
     el_options.prepend(el_user);
   });
 })(jQuery);
