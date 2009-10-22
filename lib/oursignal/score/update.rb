b5812d41fb043209b2c09eae322dcb8253477507
Shane Hanna <shane.hanna@gmail.com>
2009-10-22 16:19:43 +1100

    Oops. This explains why the velocities have been changing so fast Iguess.

--- lib/oursignal/score/update.rb
+++ lib/oursignal/score/update.rb
@@ -6,7 +6,7 @@ module Oursignal
 
       def call
         # TODO: I can build an 'OR' with DM::Query even if the class finders can't do it yet.
-        links =  Link.all # (:score_at.lt => (Time.now - 5.minutes).to_datetime)
+        links =  Link.all(:score_at.lt => (Time.now - 5.minutes).to_datetime)
         links += Link.all(:score_at => nil)
         unless links.empty?
           links.each do |link|
