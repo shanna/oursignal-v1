17ae194e1c97635cb7bcf6316b46fb8ed5e3a016
Danial Pearce <git@tigris.id.au>
2009-10-30 03:44:52 +1100

    Due to the intense cron work being done, the CPU can spike up to 80% no worries. Tell monit to ignore it unless it happens for 2 monit cycles.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ set :apache_port, '8080'
 
 set :apache_monit_test_urls, ['http://www.oursignal.com:8080/static/monit', 'http://staging.oursignal.com:8080/static/monit']
 set :varnish_monit_test_urls, ['http://www.oursignal.com/static/monit', 'http://staging.oursignal.com/static/monit']
+set :monit_cpu_user, '80% for 2 cycles'
 
 # We run our own migrations
 set :merb_use_automigrate, false
