996015e529d9f4711c621048716a113bb9d0d5a0
Shane Hanna <shane.hanna@gmail.com>
2009-11-02 16:12:28 +1100

    Daemonize scheduler.

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ namespace :deploy do
   desc 'Restart scheduler'
   task :scheduler do
     run "#{thor} os:scheduler:stop"
-    run "#{thor} os:scheduler:start"
+    run "#{thor} os:scheduler:start -d"
   end
 end
 
