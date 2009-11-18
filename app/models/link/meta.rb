08cf24616f0b7e8d85eb3c74f443dfd2bb685ce1
Shane Hanna <shane.hanna@gmail.com>
2009-11-19 10:54:15 +1100

    Use metauri titles for links.

--- app/models/link/meta.rb
+++ app/models/link/meta.rb
@@ -13,21 +13,25 @@ class Link
           next unless link
 
           begin
-            if meta.redirect? && !meta.errors?
-              if last_effective_link = Link.first(:url => URI.sanatize(meta.last_effective_uri))
-                last_effective_link.update(:meta_at => DateTime.now)
+            if !meta.errors?
+              title = meta.title.to_utf8
+              title = link.title if title.blank?
+              if meta.redirect?
+                if last_effective_link = Link.first(:url => URI.sanatize(meta.last_effective_uri))
+                  last_effective_link.update(:meta_at => DateTime.now, :title => title)
+                else
+                  last_effective_link = link.copy(
+                    :url     => URI.sanatize(meta.last_effective_uri),
+                    :title   => title,
+                    :meta_at => DateTime.now
+                  )
+                  last_effective_link.save || next
+                end
+                adapter.execute('update feed_links set link_id = ? where link_id = ?', elink.id, link.id) rescue nil
+                link.destroy
               else
-                # TODO: This eats dick and may break when you add new properties to Link.
-                # The intention here is to clone everything in the old link and set url (thus id) and meta_at.
-                last_effective_link = link.copy(
-                  :url     => URI.sanatize(meta.last_effective_uri),
-                  :title   => meta.title.to_utf8,
-                  :meta_at => DateTime.now
-                )
-                last_effective_link.save || next
+                link.update(:meta_at => DateTime.now, :title => title) unless link.meta_at
               end
-              adapter.execute('update feed_links set link_id = ? where link_id = ?', elink.id, link.id) rescue nil
-              link.destroy
             else
               link.update(:meta_at => DateTime.now) unless link.meta_at
             end
