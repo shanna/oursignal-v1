6184e0eb5945e37e8bb750d01c6f68db363834f2
Danial Pearce <git@tigris.id.au>
2010-12-21 00:09:14 +1100

    Move back to bundler07 cos bundler08 is being a dick and jesus doesn't like that.

--- bin/prettify_json.rb
+++ bin/prettify_json.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/ruby1.8
-require File.expand_path(File.join(File.dirname(__FILE__), "../gems/ruby/1.8/environment"))
-load File.expand_path(File.join(File.dirname(__FILE__), "../gems/ruby/1.8/gems/json-1.2.0/bin/prettify_json.rb"))
+require File.expand_path(File.join(File.dirname(__FILE__), "../gems/environment"))
+load File.expand_path(File.join(File.dirname(__FILE__), "../gems/gems/json-1.2.0/bin/prettify_json.rb"))
