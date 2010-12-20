58f7dabddef80cc5c16b0e34c045303aec94dbef
Shane Hanna <shane.hanna@gmail.com>
2010-12-20 11:35:47 +1100

    Resque admin.

--- config/rack.rb
+++ config/rack.rb
@@ -1,8 +1,10 @@
 # vim: syntax=ruby
 # encoding: utf-8
-require File.join(File.dirname(__FILE__), 'lib/oursignal')
+require ::File.join(::File.dirname(__FILE__), 'lib/oursignal')
 require 'oursignal/web'
 require 'oursignal/web/users'
+require 'resque/server'
 
-map('/')     { run Oursignal::Web        }
-map('/users'){ run Oursignal::Web::Users }
+map('/')            { run Oursignal::Web        }
+map('/users')       { run Oursignal::Web::Users }
+map('/admin/resque'){ run Resque::Server        }
