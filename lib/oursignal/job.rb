f75f33ab9b8c12fc86b7d3d267e83fd5b3fdaed1
Shane Hanna <shane.hanna@gmail.com>
2011-01-31 12:22:34 +1100

    Import events job base.

---
+++ lib/oursignal/job.rb
@@ -0,0 +1,45 @@
+require File.join(File.dirname(__FILE__), '..', 'oursignal')
+require 'resque'
+require 'resque/plugins/lock'
+
+# Jobs.
+require 'ev/job/rss_get'
+require 'ev/job/rss_update'
+require 'ev/job/link_update'
+
+module Oursignal
+  module Job
+
+    # Run job workers.
+    #--
+    # TODO: Env stuff to OO and Env handling to bin/ev-job.
+    # TODO: Pid files for this stuff so monit can be happy?
+    def self.start
+      # Process.daemon do # Don't deamonize while we have no stop, log or pid.
+        # TODO: Process.daemon. Master -> Slaves type deal. Master should watch the workers for that queue and restart
+        # when required.
+        queues = (ENV['QUEUES'] || ENV['QUEUE'] || '*').to_s.split(',')
+
+        begin
+          worker              = Resque::Worker.new(*queues)
+          worker.verbose      = ENV['LOGGING'] || ENV['VERBOSE']
+          worker.very_verbose = ENV['VVERBOSE']
+        rescue Resque::NoQueueError
+          abort "set QUEUE env var, e.g. $ QUEUE=critical,high rake resque:work"
+        end
+
+        puts "Starting worker #{worker}"
+        worker.log "Starting worker #{worker}"
+        worker.work(ENV['INTERVAL'] || 5) # interval, will block
+      # end
+    end
+
+    # Stop job workers.
+    #--
+    # TODO: Env stuff to OO and Env handling to bin/ev-job.
+    # TODO: Find the master, have it send QUIT to all it's workers.
+    def self.stop
+      raise NotImplementedError
+    end
+  end # Job
+end # Oursignal
