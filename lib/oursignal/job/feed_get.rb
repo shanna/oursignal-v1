a47e9b9acee6666f2b9fd297268b91a681813b65
Shane Hanna <shane.hanna@gmail.com>
2011-02-10 14:58:18 +1100

    FeedGet uses URI::IO now.

--- lib/oursignal/job/feed_get.rb
+++ lib/oursignal/job/feed_get.rb
@@ -1,8 +1,6 @@
-require 'curb'
 require 'digest/md5'
 require 'fileutils'
-require 'uri/sanitize'
-require 'zlib'
+require 'uri/io'
 
 # Business.
 require 'oursignal/feed'
@@ -10,81 +8,38 @@ require 'oursignal/feed'
 module Oursignal
   module Job
     class FeedGet
-      USER_AGENT = 'oursignal-rss/0.2 +oursignal.com'
+      USER_AGENT = 'oursignal/0.2 +oursignal.com'
 
       extend Resque::Plugins::Lock
       @queue = :feed_get
 
-      class << self
-        def perform url
-          feed = Oursignal::Feed.search(url)
+      def self.perform url
+        feed = Oursignal::Feed.search(url)
+        uri  = URI::IO.open(url) do |io|
+          io.follow_location = true
+          io.timeout         = 10
 
-          curl = http_get url, feed
-          case curl.response_code.to_s
-            when /^5/
-              # TODO: Delay retrying.
-            when /^4/
-              # TODO: Delete feed?
-            when /^3/
-              # Nothing, it's up to date.
-            when /^2/
-              etag          = etag_from_header(curl.header_str)
-              last_modified = last_modified_from_header(curl.header_str)
-              feed.update(last_modified: last_modified, etag: etag)
-
-              dir  = File.join Oursignal.root, 'tmp', 'rss'
-              path = File.join dir, Digest::MD5.hexdigest(url)
-              FileUtils.mkdir_p(dir)
-              File.open(path, 'w+'){|fh| fh.write force_encoding(decompress(curl))}
-              Resque::Job.create :feed, 'Oursignal::Job::Feed', url, path
-          end
+          io.headers['User-Agent']        = USER_AGENT
+          io.headers['If-Modified-Since'] = feed.last_modified if feed && feed.last_modified
+          io.headers['If-None-Match']     = feed.etag          if feed && feed.etag
         end
 
-        protected
-          def http_get uri, feed = nil
-            Curl::Easy.perform(uri) do |easy|
-              easy.follow_location = true
-              easy.timeout         = 10
+        case uri.status.to_s
+          when /^5/
+            # TODO: Delay retrying.
+          when /^4/
+            # TODO: Delete feed?
+          when /^3/
+            # Nothing, it's up to date.
+          when /^2/
+            feed.update(last_modified: uri.last_modified, etag: uri.etag)
 
-              easy.headers['User-Agent']        = USER_AGENT
-              easy.headers['Accept-encoding']   = 'gzip'
-              easy.headers['If-Modified-Since'] = feed.last_modified if feed && feed.last_modified
-              easy.headers['If-None-Match']     = feed.etag          if feed && feed.etag
-            end
-          end
-
-          def decompress curl
-            if curl.header_str.match(/Content-Encoding: gzip/)
-              begin
-                gz   = Zlib::GzipReader.new(StringIO.new(curl.body_str))
-                body = gz.read
-                gz.close
-                body
-              rescue Zlib::GzipFile::Error
-                curl.body_str
-              end
-            else
-              curl.body_str
-            end
-          end
-
-          def force_encoding raw, encoding = 'utf-8'
-            options = {invalid: :replace, undef: :replace}
-            raw.valid_encoding? ? raw.encode('utf-8', options) : raw.force_encoding('utf-8').encode('utf-8', options)
-          rescue => error
-            warn error.message
-            ''
-          end
-
-          def etag_from_header(header)
-            header =~ /.*ETag:\s(.*)\r/
-            $1
-          end
-
-          def last_modified_from_header(header)
-            header =~ /.*Last-Modified:\s(.*)\r/
-            Time.parse($1) if $1
-          end
+            dir  = File.join Oursignal.root, 'tmp', 'rss'
+            path = File.join dir, Digest::MD5.hexdigest(url)
+            FileUtils.mkdir_p(dir)
+            File.open(path, 'w+'){|fh| fh.write uri.read}
+            Resque::Job.create :feed, 'Oursignal::Job::Feed', url, path
+        end
       end
     end # CalendarGet
   end # Job
