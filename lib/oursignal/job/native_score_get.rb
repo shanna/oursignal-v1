8390c926c78c89a98b5e6dd92cc2ff278d272a62
Shane Hanna <shane.hanna@gmail.com>
2011-05-27 08:21:50 +1000

    Get recursive to complete TODO.

--- lib/oursignal/job/native_score_get.rb
+++ lib/oursignal/job/native_score_get.rb
@@ -10,17 +10,17 @@ module Oursignal
       extend Resque::Plugins::Lock
       @queue = :native_score_get
 
-      #--
-      # TODO: Split up multiple URLs reads.
-      def self.perform source_klass
+      def self.perform source_klass, source_url = nil
         source = Oursignal::Score::Native.find(source_klass)
-        [source.url].flatten.each do |url|
-          uri = URI::IO.open(url) do |io|
+
+        if source_url
+          uri = URI::IO.open(source_url) do |io|
             io.follow_location = true
             io.timeout         = 5
           end
-
           source.perform(uri) if uri.status.to_s =~ /^2/
+        else
+          [*source.url].each{|url| Resque::Job.create :native_score_get, self, source_klass, url}
         end
       end
     end
