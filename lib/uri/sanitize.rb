23dc76cd4a24bf6aff67d51963a955111114e8f5
Shane Hanna <shane.hanna@gmail.com>
2011-05-29 22:40:43 +1000

    Postranks URI cleaning.

--- lib/uri/sanitize.rb
+++ lib/uri/sanitize.rb
@@ -1,10 +1,10 @@
+require 'postrank-uri'
 require 'rack/utils'
-require 'uri'
 
 module URI
   module Sanitize
     def self.parse original_uri
-      uri       = URI.parse(original_uri.to_s).normalize
+      uri       = PostRank::URI.clean(original_uri.to_s, raw: true)
       params    = Hash[*Rack::Utils.parse_query(uri.query).sort.flatten]
       uri.query = Rack::Utils.build_query(params) unless params.empty?
       uri
