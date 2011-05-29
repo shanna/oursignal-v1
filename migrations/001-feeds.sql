1a0f1c6cab03a90dc1d01f689731398eea8c9980
Shane Hanna <shane.hanna@gmail.com>
2011-05-29 17:49:05 +1000

    Some feeds.

---
+++ migrations/001-feeds.sql
@@ -0,0 +1,5 @@
+insert into feeds (url, created_at, updated_at) values
+  ('http://digg.com/news.rss', now(), now()),
+  ('http://www.reddit.com/.rss', now(), now()),
+  ('http://feeds.delicious.com/v2/rss/popular', now(), now()),
+  ('http://news.ycombinator.com/rss', now(), now());
