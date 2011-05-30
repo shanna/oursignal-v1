3bec576c90ab49dbec5ef72ad17a8211d223d19b
Shane Hanna <shane.hanna@gmail.com>
2011-05-31 01:51:18 +1000

    Missing scheme attributes.

--- lib/oursignal/scheme/link.rb
+++ lib/oursignal/scheme/link.rb
@@ -4,18 +4,21 @@ module Oursignal
   class Scheme
     class Link < Scheme
       store :links
-      attribute :id,                       Swift::Type::Integer, serial: true, key: true
-      attribute :url,                      Swift::Type::String
-      attribute :title,                    Swift::Type::String
-      attribute :score_delicious,          Swift::Type::Float,   default: 0
-      attribute :score_digg,               Swift::Type::Float,   default: 0
-      attribute :score_frequency,          Swift::Type::Float,   default: 0
-      attribute :score_freshness,          Swift::Type::Float,   default: 0
-      attribute :score_reddit,             Swift::Type::Float,   default: 0
-      attribute :score_twitter,            Swift::Type::Float,   default: 0
-      attribute :score_ycombinator,        Swift::Type::Float,   default: 0
-      attribute :updated_at,               Swift::Type::Time,    default: proc{ Time.now }
-      attribute :created_at,               Swift::Type::Time,    default: proc{ Time.now }
+      attribute :id,                Swift::Type::Integer, serial: true, key: true
+      attribute :url,               Swift::Type::String
+      attribute :title,             Swift::Type::String
+      attribute :score_delicious,   Swift::Type::Float,   default: 0
+      attribute :score_digg,        Swift::Type::Float,   default: 0
+      attribute :score_facebook,    Swift::Type::Float,   default: 0
+      attribute :score_frequency,   Swift::Type::Float,   default: 0
+      attribute :score_freshness,   Swift::Type::Float,   default: 0
+      attribute :score_googlebuzz,  Swift::Type::Float,   default: 0
+      attribute :score_reddit,      Swift::Type::Float,   default: 0
+      attribute :score_twitter,     Swift::Type::Float,   default: 0
+      attribute :score_ycombinator, Swift::Type::Float,   default: 0
+      attribute :updated_at,        Swift::Type::Time,    default: proc{ Time.now }
+      attribute :created_at,        Swift::Type::Time,    default: proc{ Time.now }
+      attribute :referred_at,       Swift::Type::Time,    default: proc{ Time.now }
     end # Link
   end # Scheme
 end # Oursignal
