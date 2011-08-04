12b0ed667bb4c415675fefcd97fa78c3eec152ed
Shane Hanna <shane.hanna@gmail.com>
2011-08-05 01:55:31 +1000

    Find timesteps by id or closest step to time.

--- lib/oursignal/timestep.rb
+++ lib/oursignal/timestep.rb
@@ -1,7 +1,24 @@
+require 'date'
+
 # Schema.
-require 'oursignal/scheme/feed'
+require 'oursignal/scheme/timestep'
 
 module Oursignal
   class Timestep < Scheme::Timestep
+    def self.find id
+      case id
+        when nil                  then nil
+        when Integer              then get(id: id)
+        when Time, DateTime, Date
+          # TODO: Hrm this is going to be a table scan I think.
+          execute(%q{
+            select *, abs(date_part('epoch', created_at - ?)) as diff
+            from timesteps
+            order by diff asc
+            limit 1
+          }, id.to_time).first
+        else find(DateTime.parse(id.to_s)) rescue find(id.to_s.to_i)
+      end
+    end
   end # Timestep
 end # oursignal
