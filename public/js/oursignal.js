b76b7349b7b441ac42dc3826ad70a0cf2c82bfb1
Shane Hanna <shane.hanna@gmail.com>
2011-09-15 23:58:35 +1000

    View a timestep.
    Bare minimum required to build a treemap.

--- public/js/oursignal.js
+++ public/js/oursignal.js
@@ -1,2 +1,9 @@
 // Oursignal.
-
+$.getJSON('/timestep.json', function(links) {
+  $(function() {
+    var $timestep = $('#timestep');
+    $.each(links, function(index, link) {
+      $timestep.append($('<li/>', {'data-score': link.score}).append($('<a/>', {href: link.url, text: link.title})));
+    });
+  });
+});
