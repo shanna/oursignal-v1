0a8f17f6457e378517c5cfabb1e7d953d0c8ad02
Shane Hanna <shane.hanna@gmail.com>
2011-12-12 23:46:16 +1100

    No need for resque anymore.

--- config/rack.rb
+++ config/rack.rb
@@ -2,7 +2,5 @@
 # encoding: utf-8
 require ::File.join(::File.dirname(__FILE__), 'lib/oursignal')
 require 'oursignal/web/server'
-require 'resque/server'
 
-map('/')            { run Oursignal::Web::Server }
-map('/admin/resque'){ run Resque::Server         }
+map('/'){ run Oursignal::Web::Server }
