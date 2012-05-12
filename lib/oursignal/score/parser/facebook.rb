d18ab7426174e45d842bf94c85d3e071640372f8
Shane Hanna <shane.hanna@gmail.com>
2012-05-12 20:14:20 +1000

    Facebook seems to have issues with ' in url.

--- lib/oursignal/score/parser/facebook.rb
+++ lib/oursignal/score/parser/facebook.rb
@@ -3,14 +3,13 @@ require 'oursignal/score/parser'
 module Oursignal
   class Score
     class Parser
-      #--
-      # TODO: Facebook actually accepts a comma separated list of URLs without an API key.
-      # Make less requests by chunking URLs if we can find an upper limit.
       class Facebook < Parser
         def urls
-          urls = []
+          urls   = []
           links.each_slice(25) do |slice|
-            urls << 'http://api.ak.facebook.com/restserver.php?v=1.0&method=links.getStats&format=json&urls=' + slice.map{|link| CGI.escape(link.url)}.join(',')
+            # Facebook API has issues with ' and ". Bad luck.
+            escaped = slice.reject{|link| link.url.to_s =~ /['"]/}.map{|link| CGI.escape(link.url)}
+            urls << 'http://api.ak.facebook.com/restserver.php?v=1.0&method=links.getStats&format=json&urls=' + escaped.join(',')
           end
           urls
         end
@@ -18,11 +17,15 @@ module Oursignal
         def parse source
           data = Oj.load(source, symbol_keys: true) || return
           data.each do |entry|
-            link  = links.detect{|link| link.match?(entry[:url])} || next
-            score = entry[:share_count] || next # Also like_count, comment_count, click_count and total_count if we need it.
+            begin
+              link  = links.detect{|link| link.match?(entry[:url])} || next
+              score = entry[:share_count] || next # Also like_count, comment_count, click_count and total_count if we need it.
 
-            puts "facebook:link(#{link.id}, #{link.url}):#{score}"
-            Link.execute('update links set score_facebook = ?, updated_at = now() where id = ?', score.to_i, link.id)
+              puts "facebook:link(#{link.id}, #{link.url}):#{score}"
+              Link.execute('update links set score_facebook = ?, updated_at = now() where id = ?', score.to_i, link.id)
+            rescue => error
+              warn ['Facebook Score Error:', caller.first, error.inspect, entry.inspect].join("\n")
+            end
           end
         end
       end # Facebook
