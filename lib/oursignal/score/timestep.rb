d218e6c66a98db3fa86ad3e9cc7cf9ddc0dc7eb9
Shane Hanna <shane.hanna@gmail.com>
2012-06-07 02:33:11 +1000

    Timestep metrics and speed improvements.

--- lib/oursignal/score/timestep.rb
+++ lib/oursignal/score/timestep.rb
@@ -22,30 +22,36 @@ module Oursignal
       ]
       ORIGIN = Array.new(FIELDS.size, 0.0)
 
-      # the actual decay rate is DECAY_RATE.abs so, tweak it in the right direction.
-      DECAY_RATE = -0.25
+      # Link lifetime from referred_at.
+      LIFETIME = 1_440
 
       #--
       # TODO: Golf.
-      # TODO: Decay.
+      # TODO: Smaller methods.
+      # TODO: C++. This isn't performing well enough.
       def self.perform
         Oursignal.db.transaction do
+          now   = Time.now
           step  = Oursignal::Timestep.create
           links = Oursignal.db.execute(%Q{
             select
               id as link_id,
-              extract(epoch from now() - referred_at) / 86400 as days,
+              extract(epoch from now() - referred_at)::int/60 as minutes,
               #{FIELDS.join(', ')}
             from links
+            where referred_at > now() - interval '1 month'
           }) # Oh boy.
-
+          puts "selected %d links in\t%10.4fs" % [links.count, Time.now - now]
           raise "Timestep requires a minimum of 100 links in the DB but only #{links.count} exist." unless links.count >= 100
 
+          now     = Time.now
           result  = Flock.kcluster(100, links.map{|l| l.values_at(*FIELDS)}, seed: Flock::SEED_SPREADOUT)
           cluster = result[:cluster]
+          puts "kmeans clustered in\t%10.4fs" % [Time.now - now]
 
           # TODO: Enumerable#inject avoids awkward kscores = Hash[kscores]
           # Creates a map of cluster number to index in sorted centroids array.
+          now     = Time.now
           kscores = result[:centroid] \
             .map
             .with_index{|c, pos| [c, pos]}
@@ -53,20 +59,28 @@ module Oursignal
             .map
             .with_index{|(c, pos), idx| [pos, idx]}
           kscores = Hash[kscores]
+          puts "scores mapped in\t%10.4fs" % [Time.now - now]
 
+          now        = Time.now
+          scores_ts  = 0
+          scores_sth = Oursignal.db.prepare('select * from scores where link_id = ? order by timestep_id desc')
           links.each_with_index do |link, index|
-            kmeans  = 0.01 * (kscores[cluster[index]] + 1)
+            # Skip links older than 12 hours. Their scores will be zero by then anyway thanks to the decay.
+            # This is done purely for performance.
+            next if link[:minutes] > LIFETIME
+            scores_ts += 1
+
+            # The kmeans clustering score.
+            kmeans = 0.01 * (kscores[cluster[index]] + 1)
 
             # TODO: Can be golfed if you select just the most recent score plus a count.
-            # Might not reduce the DB load but will reduce the number of loops in Math::Ema by the count.
-            # For now use this just in case the smoothing factor needs to be changed to a fixed number.
-            sql     = 'select * from scores where link_id = ? order by timestep_id desc'
-            scores  = Oursignal.db.execute(sql, link[:link_id]).entries
+            # Will reduce the number of results (not queries) and will reduce the number of loops in Math::Ema by the
+            # count. For now use this just in case the smoothing factor needs to be changed to a fixed number.
+            scores = scores_sth.execute(link[:link_id]).entries
 
             # we got a single score just add a dummy score as first one
             # and assume this single score is the most recent one.
             scores.unshift({timestep_id: step.id, kmeans: kmeans}.merge(Hash[FIELDS.zip(ORIGIN)])) if scores.size < 2
-
 
             # TODO: Not perfect.
             displacement = Flock.euclidian_distance(link.values_at(*FIELDS), scores.first.values_at(*FIELDS))
@@ -76,13 +90,22 @@ module Oursignal
             # ema with smoothing factor 2/(N+1)
             ema = Math::Ema.new((2.0 / (scores.count + 1)), scores.shift[:kmeans]).update(scores.map{|row| row[:kmeans]})
 
-            # decay - exponential cooling.
-            score = [ema * Math.exp(DECAY_RATE * link.delete(:days)), 0].max
-            # TODO: Remove sneaky .delete() hacks and explicitly name fields to be saved.
+            # Straight linear decay. KISS.
+            decay = ema.to_f / LIFETIME
+            score = [ema - (link.delete(:minutes).to_i * decay), 0].max
+
+            # Add a score.
             Score.create({timestep_id: step.id, score: score, kmeans: kmeans, ema: ema, velocity: velocity}.merge(link))
           end
-        end
-      end
+          puts "created %d scores in\t%10.4fs" % [scores_ts, Time.now - now]
+
+          # Not enough links in timestep? Ditch it.
+          if scores_ts < 100
+            warn "Only %d scores, deleting timestep." % scores_ts
+            Oursignal.db.execute('delete from timesteps where id = ?', step.id)
+          end
+        end # .transaction
+      end # .perform
     end # Timestep
   end # Score
 end # Oursignal
