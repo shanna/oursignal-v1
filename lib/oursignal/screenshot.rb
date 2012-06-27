80094ce0dd53225b3056f58d6ba280da08c4ae41
Shane Hanna <shane.hanna@gmail.com>
2012-06-28 02:43:05 +1000

    Timeout screen shot code if we can.

--- lib/oursignal/screenshot.rb
+++ lib/oursignal/screenshot.rb
@@ -1,4 +1,5 @@
 require 'oursignal'
+require 'timeout'
 
 module Oursignal
   #--
@@ -10,17 +11,23 @@ module Oursignal
       tempfile = File.join(Oursignal.root, 'tmp', 'screenshots', filename)
       file     = File.join(Oursignal.root, 'public', 'screenshots', filename)
 
-      Dir.chdir(Oursignal.root) do
-        [
-          ['/bin/mkdir', '-p', File.dirname(tempfile)],
-          ['/bin/mkdir', '-p', File.dirname(file)],
-          ['bin/phantomjs', script, link.url, tempfile],
-          ['/usr/bin/convert', tempfile, '-crop 1680x1080+0+0', tempfile],
-          ['/usr/bin/convert', tempfile, '-filter Lanczos', '-thumbnail 336x216', file],
-          ['/bin/rm', tempfile]
-        ].each do |command|
-          system(command.join(' ')) || raise('system(%s) failed: %s' % [command.join(' '), $?])
+      begin
+        Timeout.timeout(60) do
+          Dir.chdir(Oursignal.root) do
+            [
+              ['/bin/mkdir', '-p', File.dirname(tempfile)],
+              ['/bin/mkdir', '-p', File.dirname(file)],
+              ['bin/phantomjs', script, link.url, tempfile],
+              ['/usr/bin/convert', tempfile, '-crop 1680x1080+0+0', tempfile],
+              ['/usr/bin/convert', tempfile, '-filter Lanczos', '-thumbnail 336x216', file],
+              ['/bin/rm', tempfile]
+            ].each do |command|
+              system(command.join(' ')) || raise('system(%s) failed: %s' % [command.join(' '), $?])
+            end
+          end
         end
+      rescue Timeout::Error
+        warn "Timed out screenshotting #{link.url}"
       end
     end
   end # Screenshot
